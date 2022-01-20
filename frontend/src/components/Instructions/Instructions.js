83a74894d5cd79c93f99789a7c70a7c1e3f09442
Mustapha <sml.mus.99@gmail.com>
2022-01-20 23:12:01 +0100

    :sparkle: Refactor CSS :sparkle: change font size of ingredients change meduim padding of instructions remove line height of recipe title Add section padding to Auth screens

--- frontend/src/components/Instructions/Instructions.js
+++ frontend/src/components/Instructions/Instructions.js
@@ -5,7 +5,7 @@ import './instructions.css'
 
 const Instructions = ({instructionsList}) => {
     return (
-        <Col sm={12} className="px-5 py-4 instructions">
+        <Col sm={12} className="px-md-5 py-4 instructions">
             <h3 className="instructions__header">Instructions: </h3>
             <ul className="p-3 instructions__content">
                 {instructionsList && instructionsList.map((instruction, index) => (
